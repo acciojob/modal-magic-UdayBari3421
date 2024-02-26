9f4411bfa57db5d8b4c381d994702b7c6f84a02b
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-02-26 15:10:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let modal = document.getElementById("myModal");
 let btn = document.getElementById("openModal");
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close-modal")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
